ab3051c92078283d1d4c849378abb9f6ae42470c
Niks0602 <122961328+Niks0602@users.noreply.github.com>
2023-03-07 10:28:32 +0800

    Add files via upload
    Signed-off-by: Niks0602 <122961328+Niks0602@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,228 @@
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 230) {
+        document.getElementById("sfButton").hidden = false;
+        document.getElementById("scentFinder").hidden = false;
+    }
+    if(window.scrollY < 230) {
+        document.getElementById("sfButton").hidden = true;
+        document.getElementById("scentFinder").hidden = true;
+    }
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
+const emojiBtn = document.querySelector('#emoji-btn');
+const picker = new EmojiButton();
+
+
+// Emoji selection  
+window.addEventListener('DOMContentLoaded', () => {
+
+    picker.on('emoji', emoji => {
+      document.querySelector('input').value += emoji;
+    });
+  
+    emojiBtn.addEventListener('click', () => {
+      picker.togglePicker(emojiBtn);
+    });
+  });        
+
+//   chat button toggler 
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
+
+// send msg 
+submitBtn.addEventListener('click', ()=>{
+    let userInput = inputElm.value;
+
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="img/me.jpg" class="avatar">
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
+
+})
+
+function start(){
+    document.getElementById("birdBox").hidden = true;
+    document.getElementById("initialText").hidden = true;
+    document.getElementById("startButton").style.display = 'none' ;
+
+    document.getElementById("in-msg-div").hidden = false;
+    document.getElementById("out-msg-div1").hidden = false;
+    document.getElementById("out-msg-div2").hidden = false;
+
+    document.getElementById("out-msg1").innerHTML = "For you";
+    document.getElementById("out-msg2").innerHTML = "For someone else";
+
+    document.getElementById("out-msg1").onclick = firstSFQ ;
+    document.getElementById("out-msg2").onclick = firstSFQ ;
+    
+    document.getElementById("in-msg").innerHTML = "You are choosing...";
+
+
+
+}
+
+function firstSFQ(){ 
+    document.getElementById("out-msg-div3").hidden = false;
+
+    document.getElementById("out-msg1").innerHTML = "For Him";
+    document.getElementById("out-msg2").innerHTML = "For Her";
+    document.getElementById("out-msg3").innerHTML = "For Both";
+    
+    document.getElementById("out-msg1").onclick = secondSFQ ;
+    document.getElementById("out-msg2").onclick = secondSFQ ;
+    document.getElementById("out-msg3").onclick = secondSFQ ;
+
+    document.getElementById("in-msg").innerHTML = "The fragarance is...";
+}
+
+function secondSFQ(){
+    document.getElementById("out-msg1").innerHTML = "A day to the races";
+    document.getElementById("out-msg2").innerHTML = "A night at the opera";
+    document.getElementById("out-msg3").innerHTML = "Lunch hour to happy hour";
+
+    document.getElementById("out-msg1").onclick = thirdSFQ ;
+    document.getElementById("out-msg2").onclick = thirdSFQ ;
+    document.getElementById("out-msg3").onclick = thirdSFQ ;
+    
+    document.getElementById("in-msg").innerHTML = "When will they be wearing the fragrance?";
+}
+
+function thirdSFQ(){
+    document.getElementById("out-msg-div4").hidden = false;
+    document.getElementById("out-msg-div5").hidden = false;
+
+    document.getElementById("out-msg1").innerHTML = "Fresh";
+    document.getElementById("out-msg2").innerHTML = "Lively";
+    document.getElementById("out-msg3").innerHTML = "Delicate";
+    document.getElementById("out-msg4").innerHTML = "Deep";
+    document.getElementById("out-msg5").innerHTML = "Opulent";
+
+    document.getElementById("out-msg1").onclick = fourthSFQ ;
+    document.getElementById("out-msg2").onclick = fourthSFQ ;
+    document.getElementById("out-msg3").onclick = fourthSFQ ;
+    document.getElementById("out-msg4").onclick = fourthSFQ ;
+    document.getElementById("out-msg5").onclick = fourthSFQ ;
+
+    document.getElementById("in-msg").innerHTML = "Which word would they be most drawn to?";
+}
+
+function fourthSFQ(){
+    document.getElementById("out-msg-div5").hidden = true;
+
+    document.getElementById("out-msg1").innerHTML = "Daring";
+    document.getElementById("out-msg2").innerHTML = "Fancy";
+    document.getElementById("out-msg3").innerHTML = "Relaxed";
+    document.getElementById("out-msg4").innerHTML = "Upbeat";
+
+    document.getElementById("out-msg1").onclick = fifthSFQ ;
+    document.getElementById("out-msg2").onclick = fifthSFQ ;
+    document.getElementById("out-msg3").onclick = fifthSFQ ;
+    document.getElementById("out-msg4").onclick = fifthSFQ ;
+
+    document.getElementById("in-msg").innerHTML = "You want the fragrance to make them feel..";
+}
+
+function fifthSFQ(){
+    document.getElementById("resultScents").hidden = false;
+
+}
+
+function exitButton(){
+    document.getElementById("resultScents").hidden = true;
+
+    document.getElementById("in-msg-div").hidden =  true;
+    document.getElementById("out-msg-div1").hidden = true;
+    document.getElementById("out-msg-div2").hidden = true;
+    document.getElementById("out-msg-div3").hidden = true;
+    document.getElementById("out-msg-div4").hidden = true;
+    document.getElementById("out-msg-div5").hidden = true;
+
+
+    document.getElementById("birdBox").hidden = false;
+    document.getElementById("initialText").hidden = false;
+    document.getElementById("startButton").style.display = 'inline';
+    document.getElementById("startButton").onclick = start ;
+}
+
+function readMore(){
+    document.getElementById("extra").hidden = false;
+    document.getElementById("readMore").hidden = true;
+    document.getElementById("readLess").hidden = false;
+
+}
+
+function readLess(){
+    document.getElementById("extra").hidden = true;
+    document.getElementById("readMore").hidden = false;
+    document.getElementById("readLess").hidden = true;
+}
+
+function fullScreen(){
+
+
+    document.getElementById("aFrame").height = "610px;";
+    document.getElementById("aFrame").width = "100%";
+    document.getElementById("aFrame").scrollIntoView();
+    
+    document.getElementById("fullScreenIcon").src = "Assets/minimizeIcon.png"
+    document.getElementById("fullScreenIcon").onclick = windowScreen ;
+    document.getElementById("fullScreenIcon").style = "top:1480px; right:-295px;"
+
+    disableScroll();
+}
+
+function windowScreen(){
+    document.getElementById("aFrame").height = "400px;";
+    document.getElementById("aFrame").width = "711px"
+    
+    document.getElementById("fullScreenIcon").onclick = fullScreen ;
+    document.getElementById("fullScreenIcon").src = "Assets/fullScreenIcon.png"
+    document.getElementById("fullScreenIcon").style = "top:1275px;" 
+    
+    enableScroll();
+}
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
